b7c2fdf3920283bf004854e26db26b6428a31383
Jaime Andrés Monsalve Ballesteros <70330970+andresmonsalve19@users.noreply.github.com>
2021-10-19 06:59:49 -0500

    tercer commit

---
+++ src/main/java/Asiento.java
@@ -0,0 +1,18 @@
+package test;
+
+public class Asiento {
+	//Atributos
+	String color;
+	int precio;
+	static int registro;
+	
+	//Métodos
+	void cambiarColor(String color) {
+		String colores[] = {"rojo", "verde", "amarillo", "negro", "blanco"}; 
+		
+		if (color == colores[0] || color == colores[1] || color == colores[2] || color == colores[3] || color == colores[4]) {
+			this.color = color;
+		}
+	}
+
+}
